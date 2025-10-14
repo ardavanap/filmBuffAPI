5bddc7338d6318ef8689f34e93271d031a00e922
Ardavan alipour <ardavanalipour80@gmail.com>
2025-10-14 21:30:11 +0330

    ADD: User registration added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     public function register(Request $request){
+
+        // TODO: return json for validation errors
 
         $validatedData = $request->validate([
             'name' => 'required|max:255',
@@ -16,9 +19,19 @@ class AuthController extends Controller
 
         // TODO: register users
 
+        User::firstOrNew([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ])->save();
+
+        return 'user created';
+
     }
 
     public function login(Request $request){
+
+        // TODO: return json for validation errors
 
         $validatedData = $request->validate([
             'email' => 'required|email|max:255|unique:users',
