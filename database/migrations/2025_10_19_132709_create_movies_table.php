63b757d4b13af9096b607efadb80c90c016dcbde
Ardavan alipour <ardavanalipour80@gmail.com>
2025-10-19 17:34:09 +0330

    ADD: movie and category migrations added.

---
+++ database/migrations/2025_10_19_132709_create_movies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('release_year');
+            $table->integer('duration');
+            $table->string('poster')->nullable();
+            $table->string('trailer_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
