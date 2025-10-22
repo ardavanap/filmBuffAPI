fb85e66e7d839ce82f855b9fdbf52a720e5eebb2
Ardavan alipour <ardavanalipour80@gmail.com>
2025-10-22 19:12:07 +0330

    ADD: Comment migration and model added and realtionships of the model added.

---
+++ app/Models/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'movie_id',
+        'content',
+    ];
+    public $timestamps = false;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function movie() {
+        return $this->belongsTo(Movie::class);
+    }
+}
