3f04911acbde45707f2c8968b8af460ddaf4eda9
Ardavan alipour <ardavanalipour80@gmail.com>
2025-10-23 13:26:49 +0330

    ADD: Rating routes added.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\RatingController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -18,4 +19,10 @@ Route::apiResource('movie', MovieController::class)->middleware('auth:sanctum');
 Route::prefix('movie/{id}/')->middleware("auth:sanctum")->group(function () {
     Route::GET('comment', [CommentController::class, 'show']);
     Route::POST('comment', [CommentController::class, 'store']);
+});
+
+// _____________________________________** RATING routes **________________________________ //
+Route::prefix('movie/{id}/')->group(function () {
+    Route::GET('/rating', [RatingController::class, 'show']);
+    Route::POST('/rate', [RatingController::class, 'store']);
 });
