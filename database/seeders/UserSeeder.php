3c6efe8226c368e77a5972ce7722c8a22d1a0ee1
Ardavan alipour <ardavanalipour80@gmail.com>
2025-10-25 00:13:32 +0330

    ADDED: A user added as seed for testing porposes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,14 +5,20 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
         User::factory(500)->create();
+
+        User::factory(1)
+            ->state(["name" => "matin"])
+            ->state(["email" => "matin12@example.com"])
+            ->state(["password" => Hash::make(123456)])
+            ->state(["is_admin" => true])
+            ->create();
     }
 }
