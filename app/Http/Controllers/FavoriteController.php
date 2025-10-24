6f65c2510bfb5ad381a7fc1aab1347f7e6eb5b43
Ardavan alipour <ardavanalipour80@gmail.com>
2025-10-24 16:26:44 +0330

    ADD: Method show() done. It returns user favorite movies.

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
-    //
+    public function show() {
+        $userFavorites = User::find(Auth::guard("sanctum")->user()->id)
+            ->Favorites;
+
+        return [
+            "userFavorites" => $userFavorites,
+        ];
+    }
+
+    public function store(Request $request) {
+
+    }
 }
