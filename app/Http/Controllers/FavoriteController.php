2c56487f58afae83d9cb072a7f21f21d8f48527b
Ardavan alipour <ardavanalipour80@gmail.com>
2025-10-24 21:45:30 +0330

    ADD: 'show' user favorite movies and 'store' user favorite movie methods added. 'store' also deletes favorite movie if already exists.

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
+use App\Models\Movie;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -9,15 +11,43 @@ use Illuminate\Support\Facades\Auth;
 class FavoriteController extends Controller
 {
     public function show() {
-        $userFavorites = User::find(Auth::guard("sanctum")->user()->id)
-            ->Favorites;
+
+        $userFavorites = User::find(Auth::guard("sanctum")->user()->id)->Favorites;
 
         return [
-            "userFavorites" => $userFavorites,
+            "Your favorite movies" => $userFavorites->map(fn ($movie) => [
+                "movie_id" => Movie::find($movie->movie_id)->id,
+                "title" => Movie::find($movie->movie_id)->title,
+            ]),
         ];
     }
 
-    public function store(Request $request) {
+    public function store(string $id) {
+
+        $user = User::find(Auth::guard("sanctum")->id());
+        $favoritedMovie = $user->Favorites->where("movie_id", $id)->first();
+        $movieTitle = Movie::find($id)->title;
+
+        if(!$favoritedMovie) {      // if movie isn't in favorite list
+
+            $addedFavorite = Favorite::create([
+                "user_id" => $user->id,
+                "movie_id" => $id,
+            ]);
+            return [
+                "message" => "Movie ADDED to your favorite list",
+                "movie" => $movieTitle
+            ];
+        }
+
+        else{
+            $favoritedMovie->delete();
+
+            return [
+                "message" => "Movie REMOVED from your favorite list",
+                "movie" => $movieTitle
+            ];
+        }
 
     }
 }
